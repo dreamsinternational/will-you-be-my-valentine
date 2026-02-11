0ba1082dfbb7fd465757b5c0625132ddf3ff857b
dreamsinternational <info@dreamsinternational.in>
2026-02-11 12:09:57 +0530

    Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/will-you-be-my-valentine",
-  plugins: [react()],
-  build: {
-    sourcemap: false,
-  },
+export default defineConfig(({ mode }) => {
+  const isVercel = process.env.VERCEL === "1" || process.env.VERCEL === "true";
+  const isProd = mode === "production";
+
+  return {
+    base: isVercel || !isProd ? "/" : "/will-you-be-my-valentine",
+    plugins: [react()],
+    build: {
+      sourcemap: false,
+    },
+  };
 });
